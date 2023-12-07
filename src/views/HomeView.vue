fc7b231b643003f70e2d0b689401cdcc6499a64e
Falco van der Meulen <fme@brunelitsolutions.nl>
2023-12-07 23:20:17 +0100

    Revising homepage.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,20 +1,19 @@
 <template>
   <div class="home h-full">
-    <div class="introduction-container flex flex-row h-fit w-full">
-      <div
-        class="flex flex-col w-2/3 h-full self-center bg-bgaccent rounded-3xl mr-6"
+    <div
+      class="introduction-container flex flex-col items-start h-full justify-center w-full debug-red"
+    >
+      <span class="font-bold leading-relaxed self-center text-9xl">
+        Hi there
+      </span>
+      <span class="font-bold leading-relaxed self-center text-9xl">
+        welcome to my
+      </span>
+      <span
+        class="font-bold leading-relaxed self-center text-9xl bg-gradient-to-br from-primary to-secondary bg-clip-text text-transparent"
       >
-        <p
-          class="font-bold leading-relaxed self-center sm:text-6xl md:text-7xl lg:text-8xl xl:text-9xl bg-gradient-to-br from-primary to-secondary bg-clip-text text-transparent"
-        >
-          Hello there!
-        </p>
-        <p
-          class="italic h-[6rem] self-center sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl bg-gradient-to-br from-secondary to-primary bg-clip-text text-transparent"
-        >
-          Welcome to my webpage.
-        </p>
-      </div>
+        homepage
+      </span>
     </div>
   </div>
 </template>
