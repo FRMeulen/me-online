9e3821987315cbae34c01eea6a4b39fe6084dd41
Falco van der Meulen <fme@brunelitsolutions.nl>
2023-12-28 10:45:38 +0100

    Cleaned up sidebar icons

--- src/components/SideBar/SideBarIcon.vue
+++ src/components/SideBar/SideBarIcon.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="sidebar-icon flex flex-row">
-    <font-awesome-icon class="mr-2" :icon="icon" />
-    <span>{{ buttonName }}</span>
+    <font-awesome-icon class="w-1/3 ml-2" :icon="icon" />
+    <span class="w-2/3 justify-center mr-2">{{ buttonName }}</span>
   </div>
 </template>
 
