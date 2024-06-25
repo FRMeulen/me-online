c463d05fbb596caa291437c361987c2ccb180f5e
Falco van der Meulen <fme@brunelitsolutions.nl>
2024-06-25 14:39:43 +0200

    Detailed moment component.

---
+++ src/components/experience/MomentDetails.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="details-box">
+    {{ name }}
+    <div class="w-full h-2 flex bg-white"></div>
+    {{ description }}
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref, watchEffect } from "vue";
+
+const props = defineProps({
+  name: String,
+  description: String,
+});
+
+const name = ref(props.name);
+const description = ref(props.description);
+
+watchEffect(() => {
+  name.value = props.name;
+  description.value = props.description;
+});
+</script>
