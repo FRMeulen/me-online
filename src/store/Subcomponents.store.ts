076a158334c2227ad25aa82d58a912d4430e0c4e
Falco van der Meulen <fme@brunelitsolutions.nl>
2024-07-03 11:53:20 +0200

    Fix components as shallowRef to remove overhead.

--- src/store/Subcomponents.store.ts
+++ src/store/Subcomponents.store.ts
@@ -1,9 +1,9 @@
 import BrunelProjects from "@/components/experience/Subcomponents/BrunelProjects.vue";
 import { defineStore } from "pinia";
-import { Component, computed, ref, Ref } from "vue";
+import { Component, computed, shallowRef, ShallowRef } from "vue";
 
 export const useSubcomponentsStore = defineStore("subcomponents", () => {
-  const registry: Ref<{ [id: string]: Component }> = ref({
+  const registry: ShallowRef<{ [id: string]: Component }> = shallowRef({
     brunel: BrunelProjects,
   });
 
