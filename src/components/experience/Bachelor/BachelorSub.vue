4503ae708fdc0fdcb08a652f1a377149ac2c77e5
Falco van der Meulen <fme@brunelitsolutions.nl>
2024-07-26 15:38:37 +0200

    Moved subcomponents.

---
+++ src/components/experience/Bachelor/BachelorSub.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="grid grid-rows-6 w-full bachelor-component">
+    <div
+      class="description-card mx-auto w-fit row-span-1 m-2 p-2 bg-bgaccent border-secondary border-2 rounded-xl text-center"
+    >
+      <p>
+        My passion for computers led me to the bachelor's of Computer
+        Engineering.
+      </p>
+      <p>
+        Here I learned various programming languages and hardware skills, and
+        practiced them through projects.
+      </p>
+      <p>
+        As my studies progressed I realized I am more interested in doing
+        software rather than hardware.
+      </p>
+      <p>
+        This is also reflected in my roles during the projects I did these four
+        years.
+      </p>
+    </div>
+    <div
+      class="information-part row-span-5 m-2 p-2 bg-bgaccent border-secondary border-2 rounded-xl grid grid-cols-12 grid-rows-1"
+    >
+      <div class="grid col-span-1 items-center justify-start">
+        <TriangleArrow :direction="'left'" :color="'secondary'" :size="20" />
+      </div>
+      <div class="grid col-span-10">
+        <ProjectInfo :project="project" />
+      </div>
+      <div class="grid col-span-1 items-center justify-end">
+        <TriangleArrow :direction="'right'" :color="'secondary'" :size="20" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import ProjectInfo from "@/components/experience/Bachelor/ProjectInfo.vue";
+import TriangleArrow from "@/components/shared/TriangleArrow.vue";
+
+const project = {
+  projectName: "Test",
+  imageName: "simba",
+  imagePos: "left",
+  paragraphs: ["paragraph 1", "paragraph 2"],
+  keywords: ["keyword 1", "keyword 2", "keyword 3"],
+};
+</script>
--- src/components/experience/MiddleSchool/MiddleSchoolSub.vue
+++ src/components/experience/MiddleSchool/MiddleSchoolSub.vue
@@ -9,43 +9,7 @@
         <p>The profile I went with was called Nature & Technology.</p>
         <p>This set focuses on scientific subjects.</p>
       </div>
-      <div
-        class="years-card m-2 pb-2 bg-bgaccent border-secondary border-2 rounded-xl text-center relative mb-[35px] overflow-visible"
-      >
-        <p class="text-3xl text-primary mb-4">Years</p>
-        <p>The education took a total of 6 years.</p>
-        <p>Check the subjects per year here.</p>
-        <div class="relative mb-[25px] overflow-visible">
-          <div
-            class="absolute flex flex-row justify-evenly right-5 left-5 top-2 p-2 bg-bgprimary border border-secondary rounded-2xl text-white"
-          >
-            <div
-              class="bg-secondary rounded-full w-8 h-8 flex items-center justify-center font-bold hover:bg-accent"
-              @mouseenter="selectYear(1)"
-            >
-              <span>1</span>
-            </div>
-            <div
-              class="bg-secondary rounded-full w-8 h-8 flex items-center justify-center font-bold hover:bg-accent"
-              @mouseenter="selectYear(2)"
-            >
-              <span>2</span>
-            </div>
-            <div
-              class="bg-secondary rounded-full w-8 h-8 flex items-center justify-center font-bold hover:bg-accent"
-              @mouseenter="selectYear(3)"
-            >
-              <span>3</span>
-            </div>
-            <div
-              class="bg-secondary rounded-full w-20 h-8 flex items-center justify-center font-bold hover:bg-accent"
-              @mouseenter="selectYear(4)"
-            >
-              <span>4, 5 & 6</span>
-            </div>
-          </div>
-        </div>
-      </div>
+      <YearsCard @on-select="selectYear" />
       <Transition name="fade">
         <div
           class="year-one-description m-2 pb-2 bg-bgaccent border-secondary border-2 rounded-xl text-center"
@@ -90,6 +54,7 @@
 <script lang="ts" setup>
 import { useMiddleSchoolSubjectsStore } from "@/store";
 import { computed, ref } from "vue";
+import YearsCard from "@/components/experience/MiddleSchool/YearsCard.vue";
 
 const selectedYear = ref(0);
 const store = useMiddleSchoolSubjectsStore();
