6a8faf9d881625b11a64c6786b338d1232ce8c81
Falco van der Meulen <fme@brunelitsolutions.nl>
2024-08-01 13:22:27 +0200

    Removed duplicated description paragraph.

--- src/components/experience/Bachelor/BachelorSub.vue
+++ src/components/experience/Bachelor/BachelorSub.vue
@@ -21,26 +21,6 @@
       </p>
     </div>
     <div
-      class="description-card mx-auto w-fit row-span-1 m-2 p-2 bg-bgaccent border-secondary border-2 rounded-xl text-center"
-    >
-      <p>
-        My passion for computers led me to the bachelor's of Computer
-        Engineering.
-      </p>
-      <p>
-        Here I learned various programming languages and hardware skills, and
-        practiced them through projects.
-      </p>
-      <p>
-        As my studies progressed I realized I am more interested in doing
-        software rather than hardware.
-      </p>
-      <p>
-        This is also reflected in my roles during the projects I did these four
-        years.
-      </p>
-    </div>
-    <div
       class="information-part row-span-5 m-2 p-2 bg-bgaccent border-secondary border-2 rounded-xl grid grid-cols-12 grid-rows-1"
     >
       <div class="grid col-span-1 items-center justify-start">
