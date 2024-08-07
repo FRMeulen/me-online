6095f0a64ed767b014674343c5bae7452496554f
Falco van der Meulen <fme@brunelitsolutions.nl>
2024-08-07 12:19:55 +0200

    Moved peercoach sub component.

--- src/store/Subcomponents.store.ts
+++ src/store/Subcomponents.store.ts
@@ -3,7 +3,7 @@ import BrunelProjects from "@/components/experience/Subcomponents/BrunelSub.vue"
 import GraduationSub from "@/components/experience/Subcomponents/GraduationSub.vue";
 import InternshipSub from "@/components/experience/Subcomponents/InternshipSub.vue";
 import MiddleSchoolSub from "@/components/experience/MiddleSchool/MiddleSchoolSub.vue";
-import PeercoachSub from "@/components/experience/Subcomponents/PeercoachSub.vue";
+import PeercoachSub from "@/components/experience/Peercoach/PeercoachSub.vue";
 import { defineStore } from "pinia";
 import { Component, computed, shallowRef, ShallowRef } from "vue";
 
